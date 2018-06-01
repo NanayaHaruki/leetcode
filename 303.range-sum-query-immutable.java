ee421456f1bf3f39cd9b0006ae8e1c8eb8323b40
Kenn <nishino8818@qq.com>
2018-06-01 14:07:40 +0800

    update 303.range-sum-query-immutable.java

--- 303.range-sum-query-immutable.java
+++ 303.range-sum-query-immutable.java
@@ -2,18 +2,18 @@ class NumArray {
 	int[] items;
     public NumArray(int[] nums) {
         items = nums;
+        if(items.length>1) {
+            for(int i = 1; i < items.length; i++) {
+//            将每个角标的值变更成前面所有角标的和
+//            因为题目是求i ~ j之间数的和，所以循环报完后，只要一减就行了，不需要跑N次循环
+                items[i] += items[i - 1];
+            }
+        }
     }
 
     public int sumRange(int i, int j) {
-        if (items == null) return 0;
-        if (i >= items.length || i< 0 || j<0 || j >= items.length) return 0;
-
-        int sum = 0;
-        while (i <= j) {
-            sum+=items[i];
-            i++;
-        }
-        return sum;
+        if(i==0) return items[j];
+        return items[j] - items[i-1];
     }
 }
 

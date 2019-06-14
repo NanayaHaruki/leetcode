9bb73beed10b98ae1a3d108aa3345e97f327574f
Ayanokouji-Kenn <nishino8818@qq.com>
2019-06-14 18:11:28 +0800

    更新21算法

--- 21. Merge Two Sorted Lists.py
+++ 21. Merge Two Sorted Lists.py
@@ -17,50 +17,15 @@ class ListNode:
 
 class Solution:
 
-    def join(self, pq: PriorityQueue, listNode: ListNode):
-
-        if listNode is not None:
-            pq.put_nowait(listNode.val)
-            self.join(pq, listNode.next)
-
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        res = None
-        def createListNode(pq: PriorityQueue, node: ListNode):
-            if not pq.empty():
-                if node is None:
-                    node = ListNode(pq.get_nowait())
-                else:
-                    node.val = pq.get_nowait()
-                if not pq.empty():
-                    node.next = ListNode(0)
-                    createListNode(pq, node.next)
-
-        pq = PriorityQueue()
-        self.join(pq, l1)
-        self.join(pq, l2)
-
-        if pq.empty():
-            return None
-        res = ListNode(0)
-        createListNode(pq, res)
-        return res
-
-
-def printNode(listNode: ListNode):
-    if listNode is not None:
-        print(listNode.val)
-        printNode(listNode.next)
-
+        if l1 and l2:
+            if (l1.val > l2.val): l1, l2 = l2, l1
+            self.mergeTwoLists(l1.next, l2)
+        return l1 or l2
 
 if __name__ == '__main__':
-    s = Solution()
-    l1 = ListNode(1)
-    l1.next = ListNode(2)
-    l1.next.next = ListNode(3)
+    if []:
+        print("true")
+    else:
+        print("false")
 
-    l2 = ListNode(1)
-    l2.next = ListNode(3)
-    l2.next.next = ListNode(4)
-
-    node = s.mergeTwoLists(l1, l2)
-    printNode(node)

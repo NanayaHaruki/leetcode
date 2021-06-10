48076e9d2268ca043dd150c0eaae0cced4a106da
七夜春树 <nishino8818@qq.com>
2021-06-10 10:08:03 +0800

    update 518

--- 518.coin-change-2.kt
+++ 518.coin-change-2.kt
@@ -1,13 +1,3 @@
-import kotlin.math.max
-import kotlin.math.pow
-
-fun main() {
-    val s = Solution()
-    val res = s.change(5, intArrayOf(1, 2, 5))
-    println(res)
-}
-
-
 class Solution {
     fun change(amount: Int, coins: IntArray): Int {
         // 定义：dp[i] 为总金额为i时的方案数
@@ -17,10 +7,9 @@ class Solution {
         dp[0] = 1
         for (coin in coins) {
             for (i in coin..amount) {
-               dp[i]+=dp[i-coin]
+                dp[i]+=dp[i-coin]
             }
         }
         return dp[amount]
     }
 }
-

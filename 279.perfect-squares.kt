26a4455610ed290e324c3d2d74fc51d229ae1414
七夜春树 <nishino8818@qq.com>
2021-06-11 11:51:05 +0800

    update 279

--- 279.perfect-squares.kt
+++ 279.perfect-squares.kt
@@ -1,15 +1,4 @@
-import kotlin.math.max
-import kotlin.math.pow
 import kotlin.math.sqrt
-
-fun main() {
-    val s = Solution()
-    println(2.3.toInt())
-    println(2.4.toInt())
-    println(2.5.toInt())
-}
-
-
 class Solution {
     fun numSquares(n: Int): Int {
         val dp = IntArray(n+1)
@@ -23,4 +12,3 @@ class Solution {
         return dp[n]
     }
 }
-

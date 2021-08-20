20782608b094b58f9a0b1addd474356ad3b51a12
七夜春树 <nishino8818@qq.com>
2021-08-20 10:25:49 +0800

    update 541

--- 541.reverse-string-ii.kt
+++ 541.reverse-string-ii.kt
@@ -13,4 +13,27 @@ class Solution {
         }
         return String(chars)
     }
+
+    /** 210820 */
+    fun reverseStr(s: String, k: Int): String {
+        // 每2k翻转k个字符  k个翻转，不够k全翻  再k个不翻，不够k的也不翻
+        val arr = s.toCharArray()
+        for (i in 0..arr.size step k*2){
+            reverse(arr,i,k)
+        }
+        return String(arr)
+    }
+
+    fun reverse(charArray: CharArray, index: Int, len: Int) {
+        var l = index
+        var r = l + len - 1
+        if (charArray.lastIndex<r) r = charArray.lastIndex
+        while (l<r){
+            val tmp = charArray[l]
+            charArray[l]=charArray[r]
+            charArray[r]=tmp
+            l++
+            r--
+        }
+    }
 }

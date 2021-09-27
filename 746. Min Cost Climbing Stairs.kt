6e4e24d0f705beea912559d50105dd1c64a89cfd
七夜春树 <nishino8818@qq.com>
2021-09-27 11:45:14 +0800

    70 746

---
+++ 746. Min Cost Climbing Stairs.kt
@@ -0,0 +1,13 @@
+fun minCostClimbingStairs(cost: IntArray): Int {
+    // dp[n] 表示到n的时候，需要的最小cost
+    val dp = IntArray(cost.size + 1)
+    // 可以选择从0 或 1开始，那么这2层不需要消耗
+    dp[0] = 0
+    dp[1] = 0
+    for (i in 2..cost.size) {
+        val pre1Cost = cost[i - 1]
+        val pre2Cost = cost[i - 2]
+        dp[i] = min(dp[i - 1] + pre1Cost, dp[i - 2] + pre2Cost)
+    }
+    return dp.last()
+}

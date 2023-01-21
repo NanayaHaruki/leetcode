06d4c1e4d7357e1595138456212a9c665607746c
Nagisa <242180271@qq.com>
2023-01-21 13:01:15 +0800

    1482 1824

--- src/easy/main.py
+++ src/easy/main.py
@@ -2,30 +2,28 @@ from math import ceil
 from typing import List
 
 class Solution:
-    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
-        if m>len(bloomDay)//k:
-            return -1
-        l,r,n=0,int(1e9+1),len(bloomDay)
-        while l+1<r:
-            mid=(l+r)//2
-            flowers,cnt=0,0
-            for i in bloomDay:
-                if i<=mid:
-                    flowers+=1
-                    if flowers==k:
-                        cnt+=1
-                        flowers=0
-                        if cnt==m:
-                            break
-                else:
-                    flowers=0
-            if cnt==m:
-                r=mid
-            else:
-                l=mid
-        return r
+    def minSideJumps(self, obstacles: List[int]) -> int:
+        n=len(obstacles)
+        mx=int(2**31-1)
+        dp=[[mx for _ in range(3)] for _ in range(n)]
+        dp[0][0]=1
+        dp[0][1]=0
+        dp[0][2]=1
+        for i in range(1,n):
+            if obstacles[i]:
+                dp[i][obstacles[i]-1]=mx
+                
+            dp[i][0]=min(dp[i-1][0],dp[i-1][1]+1,dp[i-1][2]+1)
+            dp[i][1]=min(dp[i-1][1],dp[i-1][0]+1,dp[i-1][2]+1)
+            dp[i][2]=min(dp[i-1][2],dp[i-1][0]+1,dp[i-1][1]+1)
+
+            if obstacles[i]:
+                dp[i][obstacles[i]-1]=mx
+        return min(dp[-1])
+            
+
 
                     
 
-i=Solution().minDays([7,7,7,7,12,7,7],2,3)
+i=Solution().minSideJumps([0,1,2,3,0])
 print(i)
---
+++ 序号/1482. 制作 m 束花所需的最少天数.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        if m>len(bloomDay)//k:
+            return -1
+        l,r,n=0,int(1e9+1),len(bloomDay)
+        while l+1<r:
+            mid=(l+r)//2
+            flowers,cnt=0,0
+            for i in bloomDay:
+                if i<=mid:
+                    flowers+=1
+                    if flowers==k:
+                        cnt+=1
+                        flowers=0
+                        if cnt==m:
+                            break
+                else:
+                    flowers=0
+            if cnt==m:
+                r=mid
+            else:
+                l=mid
+        return r

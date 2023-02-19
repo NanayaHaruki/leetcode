00c9d65e802e0dc79908756c5494d658afed498d
淇硕 <nishino8818@qq.com>
2023-02-19 11:01:44 +0800

    1792

--- src/main.py
+++ src/main.py
@@ -1,107 +1,37 @@
 from typing import List
 from collections import deque
+import heapq
 
-class Solution1:
+class Clz:
+    def __init__(self,ps,total):
+        self.ps=ps
+        self.total = total
 
-    def minimumMoves(self, g: List[List[int]]) -> int:
+    def __lt__(self,other): # heap 小根堆，小的在上面，而我要变化最大的
+        return (other.total-other.ps)*(self.total+1)*self.total > (self.total-self.ps)*(other.total+1)*self.total
 
-        step, n = 1, len(g)
-
-        vis = {(0, 0, 0)}
-
-        q = [(0, 0, 0)]  # 初始位置
-
-        while q:
-
-            tmp = q
-
-            q = []
-
-            for X, Y, S in tmp:
-                print(X,Y,S)
-
-                for t in (X + 1, Y, S), (X, Y + 1, S), (X, Y, S ^ 1):  # 直接把移动后的位置算出来
-
-                    x, y, s = t
-
-                    x2, y2 = x + s, y + (s ^ 1)  # 蛇头
-
-                    if x2 < n and y2 < n and t not in vis and g[x][y] == 0 and g[x2][y2] == 0 and (s == S or g[x + 1][y + 1] == 0):
-                        if x == n - 1 and y == n - 2:  # 此时蛇头一定在 (n-1,n-1)
-
-                            return step
-
-                        vis.add(t)
-
-                        q.append(t)
-
-            step += 1
-
-        return -1
 
 class Solution:
-    def minimumMoves(self, grid: List[List[int]]) -> int:
-        # 蛇只能从上往下 从左往右移动，用三元组(row,col,direction)
-        # row 和 col 表示蛇尾所在坐标 direction 0往右 1往下记录当前蛇的状态，bfs求解
-        q=deque()
-        q.append((0,0,0))
-        st = {(0,0,0)}
-        rows,cols,steps=len(grid),len(grid[0]),0
-        while q:
-            sz = len(q)
-            while sz:
-                (x,y,d)=q.popleft()
-                if (x,y,d)==(rows-1,cols-2,0):
-                    return steps           
-                if d:# 头朝下
-                    # 转头
-                    if y+1<cols  and not grid[x][y+1]+grid[x+1][y+1] and (x,y,0) not in st:
-                        q.append((x,y,0))
-                        st.add((x,y,0))
-                    # 朝下移动
-                    if x+2<rows and not grid[x+2][y] and (x+1,y,1) not in st:
-                        q.append((x+1,y,1))
-                        st.add((x+1,y,1))
-                    # 朝右移动
-                    if y+1<cols and not grid[x][y+1]+grid[x+1][y+1] and (x,y+1,1) not in st:
-                        q.append((x,y+1,1))
-                        st.add((x,y+1,1))
-                else:# 头朝右
-                    # 转头
-                    if x+1<rows and not grid[x+1][y]+grid[x+1][y+1] and (x,y,1) not in st:
-                        q.append((x,y,1))
-                        st.add((x,y,1))
-                    # 朝右移动
-                    if y+2<cols and not grid[x][y+2] and (x,y+1,0) not in st:
-                        q.append((x,y+1,0))
-                        st.add((x,y+1,0))
-                    # 朝下移动
-                    if x+1<rows and not grid[x+1][y]+grid[x+1][y+1] and (x+1,y,0) not in st:
-                        q.append((x+1,y,0))
-                        st.add((x+1,y,0))
-                sz-=1
-            steps+=1
-        return -1
+    def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
+        l=[Clz(*c) for c in classes]
+        heapq.heapify(l)
 
-g=[
-    [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,1,0,1,1,0,0,1,0,0,0,0,1,0,0],
-    [0,1,0,0,0,0,1,0,0,1,0,0,0,0,0],
-    [0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,1,1,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,1,0,1,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,1,0,1,0,0,1,0,0,0,1,0,0],
-    [0,0,0,0,1,0,0,0,0,0,0,0,0,1,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
-    [1,0,1,1,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
-    [1,0,0,0,0,0,1,0,0,0,1,0,0,0,1],
-    [0,0,1,0,1,0,0,0,0,0,0,0,0,0,0]]
+        # 将好学生加进通过增长率最高的班级中，不断更新通过率总和，直到用完所有好学生
+        for _ in range(extraStudents):
+            heapq.heapreplace(l,Clz(l[0].ps+1,l[0].total+1))
+        return sum(clz.ps/clz.total for clz in l) / len(classes)
 
-i = Solution().minimumMoves(g)
-print(i)
+
+class A:
+    def __init__(self,a) -> None:
+        self.a=a
+    
+    def __lt__(self,B):
+        return self.a<B.a
+
+l=[A(3),A(2)]
+heapq.heapify(l)
+print(l[0].a)
                     
                     
 

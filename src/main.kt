1c6cd48153d1b70bf325803cc6787b205ab0990f
nanaya_haruki <nishino8818@qq.com>
2023-02-24 22:05:19 +0800

    1

--- src/main.kt
+++ src/main.kt
@@ -1,17 +1,8 @@
-fun main() {
-    val i = Solution1().grayCode(5)
-    for (j in i) {
-        println(j.toString(2))
-    }
-}
+import kotlin.math.min
+import kotlin.booleanArrayOf
 
-class Solution1 {
-    fun grayCode(n: Int): List<Int> {
-        val sz = 1 shl n
-        val q = MutableList(sz) { 0 }
-        for (i in 0 until sz) {
-            q[i] = i xor (i shr 1)
-        }
-        return q
-    }
+
+fun main() {
+    val i = Solution().longestRepeating(a,b,c)
+    print(a)
 }

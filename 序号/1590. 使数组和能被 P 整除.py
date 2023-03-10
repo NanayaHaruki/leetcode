39de8f47614ef6600fc00d13d0ba06cba7968506
nanaya_haruki <nishino8818@qq.com>
2023-03-10 10:03:34 +0800

    1590

--- 序号/1590. 使数组和能被 P 整除.py
+++ 序号/1590. 使数组和能被 P 整除.py
@@ -1,9 +1,3 @@
-from typing import List
-from collections import Counter,defaultdict
-import sys
-import time
-import functools
-
 class Solution:
     def minSubarray(self, nums: List[int], p: int) -> int:
         '''
@@ -24,8 +18,3 @@ class Solution:
                 ans=min(ans,i-d[pre])
             d[ps[i]%p]=i
         return -1 if ans ==n else ans
-
-
-
-i = Solution().minSubarray([8,32,31,18,34,20,21,13,1,27,23,22,11,15,30,4,2],148)
-print(i)

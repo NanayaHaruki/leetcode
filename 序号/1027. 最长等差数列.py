904f7d0a668a9b7940ac0efcdfd00e5f3b94838e
nanaya_haruki <nishino8818@qq.com>
2023-04-22 10:33:23 +0800

    1027

---
+++ 序号/1027. 最长等差数列.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestArithSeqLength(self, nums: List[int]) -> int:
+        n=len(nums)
+        dp=[{} for _ in range(n)]
+        ans=0
+        for i in range(n):
+            for j in range(i):
+                diff=nums[j]-nums[i]
+                if diff in dp[j]:
+                    dp[i][diff]=dp[j][diff]+1
+                else:
+                    dp[i][diff]=2
+                ans=max(ans,dp[i][diff])             
+        return ans

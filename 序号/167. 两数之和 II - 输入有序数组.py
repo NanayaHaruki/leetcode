faf322901d7bd39037c1815e6437ddda940dcd19
nanaya_haruki <nishino8818@qq.com>
2023-07-08 12:36:09 +0800

    167

--- 序号/167. 两数之和 II - 输入有序数组.py
+++ 序号/167. 两数之和 II - 输入有序数组.py
@@ -1,18 +1,19 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # 找和为target的两个数
-        # 方法一，从左到右遍历，固定一个数，二分找另一个数；nlogN
+        # d={}
         # for i,x in enumerate(numbers):
-        #     y = target-x
-        #     j=bisect.bisect_left(numbers,y,i+1)
-        #     if j==len(numbers) or numbers[j]!=y:
-        #         continue
-        #     else :
-        #         return [i+1,j+1]
+        #     if target-x in d:
+        #         return [d[target-x]+1,i+1]
+        #     d[x]=i
 
-        # 方法二 遍历到i时，在字典中找有没有另一个数
-        d={}
-        for i,x in enumerate(numbers):
-            if target-x in d:
-                return [d[target-x]+1,i+1]
-            d[x]=i
+        n=len(numbers)
+        l,r=0,n-1
+        while l<r:
+            s=numbers[l]+numbers[r]
+            if s==target:
+                return [l+1,r+1]
+            if s<target:
+                l+=1
+            else:
+                r-=1
+        return []

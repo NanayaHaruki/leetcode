d8a2f6792e3daed5bdf4dc8af5107d91df4c167d
nanaya_haruki <nishino8818@qq.com>
2023-08-05 09:19:22 +0800

    21

--- 序号/21. Merge Two Sorted Lists.py
+++ 序号/21. Merge Two Sorted Lists.py
@@ -22,10 +22,15 @@ class Solution:
             if (l1.val > l2.val): l1, l2 = l2, l1
             self.mergeTwoLists(l1.next, l2)
         return l1 or l2
+    
+        # if not list1:
+        #     return list2
+        # if not list2:
+        #     return list1
+        # if list1.val>list2.val:
+        #     list1,list2=list2,list1
+        # list1.next=self.mergeTwoLists(list1.next,list2)
+        # return list1
 
-if __name__ == '__main__':
-    if []:
-        print("true")
-    else:
-        print("false")
+
 

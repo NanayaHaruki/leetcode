46889758a6778d927e3b2ba384417e5c8fa21f8d
nanaya_haruki <nishino8818@qq.com>
2023-12-18 09:36:05 +0800

    162

--- 序号/162. 寻找峰值.py
+++ 序号/162. 寻找峰值.py
@@ -1,15 +1,25 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        n=len(nums)
-        if n==1:
-            return 0
-        l,r=-1,n
+        l,r=-1,len(nums)
+        if r==1:return 0
         while l+1<r:
-            m=(l+r)//2
-            if m==n-1:
-                return m
-            if nums[m]>nums[m+1]:
-                r=m
-            else:
-                l=m
+            mid=(l+r)//2
+            if mid==len(nums)-1 or nums[mid]>nums[mid+1]:
+                r=mid
+            else: # 题目保证了不可能相等
+                l=mid
         return r
+    # def findPeakElement(self, nums: List[int]) -> int:
+    #     n=len(nums)
+    #     if n==1:
+    #         return 0
+    #     l,r=-1,n
+    #     while l+1<r:
+    #         m=(l+r)//2
+    #         if m==n-1:
+    #             return m
+    #         if nums[m]>nums[m+1]:
+    #             r=m
+    #         else:
+    #             l=m
+    #     return r

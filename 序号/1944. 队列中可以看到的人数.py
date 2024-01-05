b6fa7193010ba41424edad3f17709c41b8476fd5
nanaya_haruki <nishino8818@qq.com>
2024-01-05 10:36:17 +0800

    1944

---
+++ 序号/1944. 队列中可以看到的人数.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canSeePersonsCount(self, heights: List[int]) -> List[int]:
+        # 单调栈 弹出前面比cur矮的，被弹的能看到的人数，就是自己和cur索引差
+        st=[]
+        n=len(heights)
+        ans=[0]*n
+        for i,h in enumerate(heights):
+            while st and heights[st[-1]]<h:
+                ans[st.pop()]+=1
+            if st:
+                ans[st[-1]]+=1
+            st.append(i)
+        return ans
--- 序号/2397. 被列覆盖的最多行数.py
+++ 序号/2397. 被列覆盖的最多行数.py
@@ -8,7 +8,8 @@ class Solution:
         g=[0]*m
         for i,row in enumerate(matrix):
             for j,x in enumerate(row):
-                g[i]|=x<<(n-1-j)
+                  # g[i]|=x<<(n-1-j)
+                g[i]|=x<<j # 倒过来也无所谓
         
         while cur < limit:
             # 遍历每一行，看能覆盖多少
@@ -20,7 +21,9 @@ class Solution:
             # 找下一个cur
             lb=cur&-cur
             r=cur+lb
-            cur=((r^cur)>> (lb&-lb).bit_length()+1)|r
+            # 右移后导0的个数+2
+            # python没有c++ 的 __builtin_ctz() ，java的 numberOfTrailingZeros，利用 x&-x 找最小的1，就可以找到后导0+1的个数了
+            cur=((r^cur)>> (lb&-lb).bit_length()+1)|r 
         return ans
 
 

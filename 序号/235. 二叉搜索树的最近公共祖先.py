5b57522965b42d72cd4f7915e77b05f19944a6e0
nanaya_haruki <nishino8818@qq.com>
2024-02-25 10:26:52 +0800

    235

--- 序号/235. 二叉搜索树的最近公共祖先.py
+++ 序号/235. 二叉搜索树的最近公共祖先.py
@@ -5,3 +5,14 @@ class Solution:
         if p.val > root.val and q.val > root.val:
             return self.lowestCommonAncestor(root.right,p,q)
         return root
+    
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if p.val > q.val:p,q=q,p
+        def dfs(node):
+            if not node:return None
+            if p.val == node.val or q.val==node.val:return node
+            if p.val < node.val and q.val < node.val:return dfs(node.left)
+            if p.val > node.val and q.val > node.val:return dfs(node.right)
+            if p.val < node.val and q.val > node.val:return node
+        return dfs(root)

8ee24967f8da04ae31bd8c50b073c78a2bab186a
nanaya_haruki <nishino8818@qq.com>
2024-04-05 09:23:08 +0800

    1026

--- 序号/1026. 节点与其祖先之间的最大差值.py
+++ 序号/1026. 节点与其祖先之间的最大差值.py
@@ -6,6 +6,35 @@
 #         self.right = right
 class Solution:
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        ans = 0
+        def dfs(x,mx,mn):
+            if not x: # 递归到空，这条线的极值就是参数了，更新答案
+                nonlocal ans
+                ans = max(ans,mx-mn)
+            mx=max(mx,x.val)
+            mn=min(mn,x.val)
+            dfs(x.val,mx,mn)
+            dfs(x.val,mx,mn)
+        dfs(root,root.val,root.val)
+        return ans
+    
+        def dfs(x):
+            mx = mn = x.val
+            diff = 0
+            if x.left:
+                lmx,lmn,ldiff = dfs(x.left)
+                mx=max(mx,lmx)
+                mn=min(mn,lmn)
+                diff = max(abs(x.val-lmx),abs(x.val-lmn),ldiff)
+                # print(x.val,diff)
+            if x.right:
+                rmx,rmn,rdiff = dfs(x.right)
+                mx=max(mx,rmx)
+                mn=min(mn,rmn)
+                diff = max(abs(x.val-rmx),abs(x.val-rmn),diff,rdiff)
+                # print(x.val,diff)
+            return (mx,mn,diff)
+        return dfs(root)[2]
         
         ans=[0]
         def dfs(node):

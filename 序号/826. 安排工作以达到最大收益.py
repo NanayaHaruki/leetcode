a351fb037a7225af278c2ff7acea5bb2792ef500
nanaya_haruki <nishino8818@qq.com>
2024-05-17 09:51:33 +0800

    826

--- 序号/826. 安排工作以达到最大收益.py
+++ 序号/826. 安排工作以达到最大收益.py
@@ -1,3 +1,21 @@
+# 240517 
+class Solution:
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        m,n=len(worker),len(difficulty)
+        p = list(zip(difficulty,profit))
+        p.sort()
+        mxp = [0]*n # mxp[i]为[0,i]范围内的最大利润，通过工人的能力二分得出i，再通过这个数组拿到最大利润的工作
+        mxp[0]=p[0][1]
+        for i in range(1,n):
+            mxp[i]=max(mxp[i-1],p[i][1])
+        ans = 0
+        for w in worker:
+            idx = bisect.bisect_right(p,(w,inf))
+            if idx==0:continue
+            ans+=mxp[idx-1]
+        return ans
+
+
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
         m,n=len(worker),len(difficulty)

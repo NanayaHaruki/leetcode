c15527ed042ebdb37ab376749fe48fcc78634e0f
nanaya_haruki <nishino8818@qq.com>
2024-05-29 09:37:59 +0800

    2981

---
+++ 序号/2981. 找出出现至少三次的最长特殊子字符串 I.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        n=len(s)
+        m={}
+        for i in range(n-2,0,-1): #i为长度
+            m.clear()
+            for j in range(0,n-i+1):
+                cs = set(s[j:j+i])
+                if len(cs)!=1:continue
+                c=s[j]
+                if c not in m:
+                    m[c]=1
+                else:
+                    if m[c]==2:
+                        print(c)
+                        return i
+                    else:
+                        m[c]+=1
+        return -1

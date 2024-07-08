ab684fa50d67d892c0c8d33527007b2cff00ac06
nanaya_haruki <nishino8818@qq.com>
2024-07-08 09:07:07 +0800

    724

--- 序号/724. 寻找数组的中心下标.py
+++ 序号/724. 寻找数组的中心下标.py
@@ -8,3 +8,16 @@ class Solution:
                 return i
             pre+=x
         return -1
+
+# 24.7.8   
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        ps = list(accumulate(nums))
+        for i in range(len(nums)):
+            if i==0:
+                if ps[-1]-ps[0]==0:
+                    return 0
+            else:
+                if ps[i]-nums[i]==ps[-1]-ps[i]:
+                    return i
+        return -1

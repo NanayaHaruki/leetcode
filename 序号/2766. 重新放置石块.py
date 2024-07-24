4b824e8cf43f4dab7a07dd956463a65057ffab7b
nanaya_haruki <nishino8818@qq.com>
2024-07-24 09:01:52 +0800

    2766

---
+++ 序号/2766. 重新放置石块.py
@@ -0,0 +1,7 @@
+class Solution:
+    def relocateMarbles(self, nums: List[int], moveFrom: List[int], moveTo: List[int]) -> List[int]:
+        st = set(nums)
+        for f,t in zip(moveFrom,moveTo):
+            st.remove(f)
+            st.add(t)
+        return sorted(st)

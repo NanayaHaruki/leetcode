90c3d835a8fac5e8d0f98e67fac3d67a3e0d77fc
nanaya_haruki <nishino8818@qq.com>
2024-08-12 20:15:35 +0800

    676 更新字典树解法

--- 序号/676. 实现一个魔法字典.kt
+++ 序号/676. 实现一个魔法字典.kt
@@ -1,3 +1,54 @@
+class Node {
+    var isFinished = false
+    val children = Array<Node?>(26) { null }
+}
+
+class MagicDictionary() {
+    val root = Node()
+    fun buildDict(dictionary: Array<String>) {
+        var p = root
+        for (word in dictionary) {
+            p = root
+            for (c in word) {
+                if (p.children[c - 'a'] == null) {
+                    p.children[c - 'a'] = Node()
+                }
+                p = p.children[c - 'a']!!
+            }
+            p.isFinished = true
+        }
+    }
+
+    private fun dfs(node: Node, word: String, i: Int, isChanged: Boolean): Boolean {
+        val c = word[i]
+        if (i == word.length - 1) {
+            if (node.children[c - 'a']?.isFinished == true && isChanged)
+                return true
+            for (j in 0 .. 25) {
+                if (!isChanged && j != c - 'a' && node.children[j] != null && node.children[j]!!.isFinished)
+                    return true
+            }
+            return false
+        }
+        if (node.children[c - 'a'] != null && dfs(node.children[c - 'a']!!, word, i + 1, isChanged)) {
+            return true
+        }
+        if (isChanged) return false
+        for (j in 0 .. 25) {
+            val child = node.children[j]
+            if (child == null || j==c-'a') continue
+            if (dfs(child, word, i + 1, true))
+                return true // 换这个字符ok，直接返回;否则继续换别的字符
+        }
+        return false
+
+    }
+
+    fun search(searchWord: String): Boolean {
+        return dfs(root, searchWord, 0, false)
+    }
+}
+
 class MagicDictionary() {
     private lateinit var d:Array<String>
     fun buildDict(dictionary: Array<String>) {

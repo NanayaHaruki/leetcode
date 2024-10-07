4b01de29dc1ac60d65f106b3e3cd6bd6d9c809cc
nanaya_haruki <nishino8818@qq.com>
2024-10-07 11:30:58 +0800

    871

--- 序号/871.最低加油次数.kt
+++ 序号/871.最低加油次数.kt
@@ -15,3 +15,29 @@ fun minRefuelStops(target: Int, startFuel: Int, stations: Array<IntArray>): Int 
   }
   return ans
 }
+
+/**
+ * 2024/10/07
+ * 
+*/
+class Solution {
+  fun minRefuelStops(target: Int, startFuel: Int, stations: Array<IntArray>): Int {
+      var remainFuel = startFuel // 还剩多少油
+      var cnt = 0 // 加油的次数
+      val pq = PriorityQueue<Int>(){i,j-> j-i}
+      var i = 0
+      while(remainFuel<target){
+          while(i<stations.size && stations[i][0]<=remainFuel){
+              pq.offer(stations[i][1])
+              i++
+          }
+          if(pq.isNotEmpty()){
+              remainFuel+=pq.poll()
+              cnt++
+          }else{
+              return -1
+          }
+      }
+      return cnt
+  }
+}

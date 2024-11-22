e554f5d95a2cabb729f792d9f1ea9e6e796379a8
nanaya_haruki <nishino8818@qq.com>
2024-11-22 09:40:08 +0800

    3233-1

--- 序号/3233. 统计不是特殊数字的数字数量.kt
+++ 序号/3233. 统计不是特殊数字的数字数量.kt
@@ -1,31 +1,50 @@
 class Solution {
+  
+  val primes = mutableListOf<Int>()
+  init {
+    val n = sqrt(1e9).toInt()
+    val isPrimes = BooleanArray(n+1){true}
+    for (i in 2..n){
+      if (isPrimes[i]) primes.add(i)
+      for(j in primes){
+        if(i*j>n) break
+        isPrimes[i*j]=false
+        if (i%j==0) break
+      }
+    }
+  }
+  
+  fun less(x:Int):Int{
+    var l = -1
+    var r = primes.size
+    var m = 0
+    while(l+1<r){
+      m = (l+r)/2
+      if(primes[m]*primes[m]<=x){
+        l=m
+      }else{
+        r=m
+      }
+    }
+    return l
+  }
+  fun greater(x:Int):Int{
+    var l = -1
+    var r = primes.size
+    var m = 0
+    while(l+1<r){
+      m = (l+r)/2
+      if(primes[m]*primes[m]<x){
+        l=m
+      }else{
+        r=m
+      }
+    }
+    return r
+  }
   fun nonSpecialCount(l: Int, r: Int): Int {
-      val primes = sieveOfEratosthenes(sqrt(r.toDouble()).toInt())
-      var cnt = 0
-      for (p in primes) if (p * p in l .. r) cnt++
-      return r - l + 1 - cnt
+    // 符合条件的只能是平方数
+    return r-l+1-(less(r)-greater(l)+1)
   }
 
-  private fun sieveOfEratosthenes(n: Int): List<Int> {
-      val isPrime = BooleanArray(n + 1) { true }
-      isPrime[0] = false
-      isPrime[1] = false
-
-      for (p in 2..Math.sqrt(n.toDouble()).toInt()) {
-          if (isPrime[p]) {
-              for (i in p * p..n step p) {
-                  isPrime[i] = false
-              }
-          }
-      }
-
-      val primes = mutableListOf<Int>()
-      for (i in 2..n) {
-          if (isPrime[i]) {
-              primes.add(i)
-          }
-      }
-
-      return primes
-  }
 }

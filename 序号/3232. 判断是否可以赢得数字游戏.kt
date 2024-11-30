ad1959329073ed8285b5d0e7cf5f913c9eba34f8
nanaya_haruki <nishino8818@qq.com>
2024-11-30 09:49:52 +0800

    3232

---
+++ 序号/3232. 判断是否可以赢得数字游戏.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun canAliceWin(nums: IntArray): Boolean {
+        var a =0
+        var b = 0
+        for(x in nums){
+            if(x<10) a+=x
+            else b+=x
+        }
+        return a!=b
+    }
+}

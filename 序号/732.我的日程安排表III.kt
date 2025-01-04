46fabcceebc398e285a8a8afa0a59c2110b73d79
nanaya_haruki <nishino8818@qq.com>
2025-01-04 09:06:32 +0800

    732

--- 序号/732.我的日程安排表III.kt
+++ 序号/732.我的日程安排表III.kt
@@ -13,3 +13,25 @@ class MyCalendarThree() {
     return ans
   }
 }
+/** 25/01/04 */
+class MyCalendarThree() {
+  private val d = TreeMap<Int,Int>()
+  fun book(startTime: Int, endTime: Int): Int {
+      d[startTime]=d.getOrDefault(startTime,0)+1
+      d[endTime]=d.getOrDefault(endTime,0)-1
+      var cur = 0
+      var mx = 0
+      for(v in d.values){
+          cur+=v
+          if(cur>mx) mx = cur
+      }
+      return mx
+  }
+
+}
+
+/**
+* Your MyCalendarThree object will be instantiated and called as such:
+* var obj = MyCalendarThree()
+* var param_1 = obj.book(startTime,endTime)
+*/

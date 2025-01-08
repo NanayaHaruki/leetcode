81f8d3cf934c7c125ddeae39d72afe1c4cb5ab6e
nanaya_haruki <nishino8818@qq.com>
2025-01-08 09:13:01 +0800

    2264

---
+++ 序号/2264. 字符串中最大的 3 位相同数字.kt
@@ -0,0 +1,13 @@
+class Solution {
+  fun largestGoodInteger(num: String): String {
+      var c = '0'-1
+      for(i in 2 until num.length){
+          if(num[i]==num[i-1] && num[i]==num[i-2]){
+              if(num[i]>c){
+                  c=num[i]
+              }
+          }
+      }
+      return if(c=='0'-1) "" else "$c$c$c"
+  }
+}

44106a666bad016fb022d98888f9489740a9e104
Nagisa <242180271@qq.com>
2025-01-23 09:37:18 +0800

    2920

--- 序号/2920. 收集所有金币可获得的最大积分.py
+++ 序号/2920. 收集所有金币可获得的最大积分.py
@@ -1,9 +1,3 @@
-from typing import List
-from collections import Counter, defaultdict, deque
-import math
-import itertools
-from functools import cache
-
 class Solution:
     def maximumPoints(self, edges: List[List[int]], coins: List[int], k: int) -> int:
         n = len(coins)
@@ -22,11 +16,4 @@ class Solution:
                         y+=dfs(i,j,a+1)
             return max(x,y)
         return dfs(-1,0,0)
-
-s = [1,2,3]
-print(sum(i for i in s if i!=2))
-a=[[0,1],[1,2],[2,3]]
-b=[10,10,3,3]
-
-c = Solution().maximumPoints(a,b,5)
-print(c)
+    

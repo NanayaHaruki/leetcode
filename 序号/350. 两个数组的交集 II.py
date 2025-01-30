8b5933ff5282a064320684d8d0f9815953e8285b
Nagisa <242180271@qq.com>
2025-01-30 22:09:54 +0800

    350

--- 序号/350. 两个数组的交集 II.py
+++ 序号/350. 两个数组的交集 II.py
@@ -1,3 +1,23 @@
+'''
+25/01/30
+'''
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        m,n = len(nums1),len(nums2)
+        nums1.sort()
+        nums2.sort()
+        i=j=0
+        ans = []
+        while i<m and j<n:
+            if nums1[i]==nums2[j]:
+                ans.append(nums1[i])
+                i+=1
+                j+=1
+            elif nums1[i]>nums2[j]:
+                j+=1
+            else:
+                i+=1
+        return ans
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         nums1.sort()

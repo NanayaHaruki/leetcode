b695ae53ffa295de3ebc9a2f3856fd31a6610eb3
nanaya_haruki <nishino8818@qq.com>
2025-04-01 09:18:56 +0800

    2140

---
+++ 序号/2140. 解决智力问题.kt
@@ -0,0 +1,13 @@
+class Solution {
+  fun mostPoints(questions: Array<IntArray>): Long {
+      // 从左往右遍历，遍历到i的时候无法知道从前面的哪个跳过来
+      // 从右往左遍历，i的最大值就是i+skip+1+cur
+      val n = questions.size
+      val dp = LongArray(n+1)
+      for(i in n-1 downTo 0){
+          val (cur,skip) = questions[i]
+          dp[i]=max(dp[i+1],cur+if(i+1+skip>=n) 0 else dp[i+1+skip])
+      }
+      return dp[0]
+  }
+}

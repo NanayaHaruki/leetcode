ced5c6c40f6c15246f921f64f897ede2feb953dc
nanaya_haruki <nishino8818@qq.com>
2025-05-08 08:58:42 +0800

    3342

---
+++ 序号/3342. 到达最后一个房间的最少时间 II.kt
@@ -0,0 +1,28 @@
+class Solution {
+  fun minTimeToReach(moveTime: Array<IntArray>): Int {
+      val m = moveTime.size
+      val n = moveTime[0].size
+      val q = PriorityQueue<IntArray>{a,b-> a[0]-b[0] }.apply { add(intArrayOf(0,0,0)) }
+      val dis = Array(m){IntArray(n){Int.MAX_VALUE} }
+      dis[0][0]=0
+      val ds = intArrayOf(-1,0,1,0,-1)
+      while(q.isNotEmpty()){
+          val (d,r,c) = q.poll()
+          if(d>dis[r][c]) continue
+          // 更新这个点四周的到达时间
+          val time = (r+c)%2+1
+          for(di in 0 until 4){
+              val nr = r + ds[di]
+              val nc = c + ds[di+1]
+              if (nr<0 || nr == m || nc < 0 || nc == n) continue
+              val newDis = d.coerceAtLeast(moveTime[nr][nc])+time
+              if(newDis<dis[nr][nc]){
+                  dis[nr][nc]=newDis
+                  q.offer(intArrayOf(newDis,nr,nc))
+              }
+          }
+
+      }
+      return dis.last().last()
+  }
+}
